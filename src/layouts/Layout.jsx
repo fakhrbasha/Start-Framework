afd205f803b9e17817cf05c26367c129571af055
fakhrbasha <fakhrbasha8@gmail.com>
2025-07-22 19:00:29 +0300

    updated

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import Copyright from '../components/Copyright'
 
 export default function Layout() {
